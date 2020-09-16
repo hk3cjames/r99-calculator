275700fce611d4c6d3d4c02f9ad9b558644861f5
KBS-james <37357237+KBS-james@users.noreply.github.com>
2020-09-17 00:31:59 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const fs = require("fs");
+const port = 3000;
+const server = http.createServer(function (req, res) {
+	res.writeHead(200, { "Content-Type": "text/html" });
+	fs.readFile("james_calculator.html", function (error, data) {
+		if (error) {
+			res.writeHead(404);
+			res.write("Error: File not found");
+		} else {
+			res.write(data);
+		}
+		res.end();
+	});
+});
+
+server.listen(port, function (error) {
+	if (error) {
+		console.log("something wrong", error);
+	} else {
+		console.log("server is listening on port " + port);
+	}
+});
